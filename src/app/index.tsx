eecd0c386abeacb669d0d7694b91401b8ea7fe18
heartlandbrad <Brad@heartlandracingco.com>
2025-11-24 21:27:21 -0600

    Create index.tsx

---
+++ src/app/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+function HomePage() {
+  return (
+    <div>
+      <Head>
+        <title>My Backend POC App</title>
+      </Head>
+      <h1>Welcome to my backend-poc app!</h1>
+    </div>
+  );
+}
+
+export default HomePage;
