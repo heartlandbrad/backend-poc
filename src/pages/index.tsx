5768de95f974060d07261e1605caae57feca69a5
heartlandbrad <Brad@heartlandracingco.com>
2025-11-24 23:41:17 -0600

    refactor

--- src/pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import Head from 'next/head';
-
-function HomePage() {
-  return (
-    <div>
-      <Head>
-        <title>My Backend POC App</title>
-      </Head>
-      <h1>Welcome to my backend-poc app!</h1>
